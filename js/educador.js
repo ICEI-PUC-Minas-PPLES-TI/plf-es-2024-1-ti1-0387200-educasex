537dd3a139edc40e519d0b520b9301b7f5c53b2a
Pedro Lucca <pedrolfo2911@gmail.com>
2024-05-19 15:48:21 -0300

    JSON Responsivo v2

--- js/educador.js
+++ js/educador.js
@@ -8,5 +8,15 @@ fetch('../data/educador.json')
     document.querySelector('.alunos').textContent = `${data.StudentsServed}`;
     document.querySelector('.nota').textContent = `${data.score}`;
     document.querySelector('.bio').textContent = `${data.bio}`;
+    document.querySelector('.AulaSemana0').textContent = `${data.ClasseWeek0}`;
+    document.querySelector('.AulaHorario0').textContent = `${data.ClasseTime0}`;
+    document.querySelector('.AulaTema0').textContent = `${data.ClasseTopic0}`;
+    document.querySelector('.AulaSemana1').textContent = `${data.ClasseWeek1}`;
+    document.querySelector('.AulaHorario1').textContent = `${data.ClasseTime1}`;
+    document.querySelector('.AulaTema1').textContent = `${data.ClasseTopic1}`;
+    document.querySelector('.AulaSemana2').textContent = `${data.ClasseWeek2}`;
+    document.querySelector('.AulaHorario2').textContent = `${data.ClasseTime2}`;
+    document.querySelector('.AulaTema2').textContent = `${data.ClasseTopic2}`;
+  
   })
   .catch(error => console.error('Erro ao carregar dados:', error));
